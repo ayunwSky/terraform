7b68cba048b2d966f546a0c951c999fd106f868c
ayunwSky <ayunwSky@163.com>
2022-09-21 16:30:11 +0800

    feat: 增加注释

--- alicloud/create_ram_user/output.tf
+++ alicloud/create_ram_user/output.tf
@@ -5,6 +5,7 @@ output "ram_user_name" {
 
 output "ram_user_password" {
   value       = alicloud_ram_login_profile.profile.password
+  # 命令行输出中隐藏 password 的值(其实也可以注释掉这部分不输出)
   sensitive   = true
   description = "alicloud ram user password"
 }
