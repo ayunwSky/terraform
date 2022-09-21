8ae5d655dd5761ad900f2c0fac23c99d58f152c2
ayunwSky <ayunwsky@qq.com>
2022-09-21 21:57:57 +0800

    feat: add .gitignore file

--- 01_learn-variables/setup_nginx.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-apt update
-apt -y install nginx
-echo Create: ${time} | sudo tee /var/www/html/index.html
--- alicloud/create_ram_user/variables.tf
+++
@@ -1,7 +0,0 @@
-variable "ram_user_password" {
-  type        = string
-  # false 表示确保变量值在模块内永远不会为空
-  nullable    = false
-  default     = "Pwd123!"
-  description = "alicloud ram user password"
-}
